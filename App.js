e2d4360e7102025fc886020c4db1c2a3d0af7a63
Prakashreddy000 <Prakash>
2023-09-04 23:29:24 +0530

    header

--- App.js
+++ App.js
@@ -1,2 +1,55 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
+
+
+const Header =()=>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="img" src="https://img.freepik.com/premium-vector/good-food-logo-design_79169-10.jpg?w=2000"/>
+               
+                </div>
+                <div className="header-items">
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Contact us</li>
+                        <li>Read more</li>
+                    </ul>
+
+               
+
+            </div>
+        </div>
+    );
+};
+
+
+const Body = () =>{
+    return(
+      <div className="body"> 
+        <div className="search">
+            search
+        </div>
+        <div className="res-container">
+
+        </div>
+      </div>  
+    );
+};
+
+
+
+
+const AppLayout = ()=>{
+    return(
+        <div className="app">
+            <Header/>
+        </div>
+    );
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<AppLayout/>);
